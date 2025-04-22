7f4bf0b53cc7cf965bfbb26de794a52e55090cb5
GaryLore <garyperez1369@gmail.com>
2025-04-21 20:44:12 -0400

    Create createNumPy.py
    creates dataset in form neural network keras needs

---
+++ createNumPy.py
@@ -0,0 +1,84 @@
+from fenConversion import *
+import numpy as np
+import csv
+
+eval_input = []
+eval_output = []
+
+i=0
+with open('top-chess-positions.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)
+
+    for info in reader:
+
+        #print(info[0])
+        f = FENParser(info[0])
+        vector = f.parse()
+        #print(vector)
+        mate_num = int(info[1])
+        #print(mate_num)
+
+        eval_input.append(vector)
+        eval_output.append(mate_num)
+        i += 1
+        if i % 10000== 0:
+            print(i/1000000)
+
+eval_X = np.array(eval_input)
+eval_Y = np.array(eval_output)
+
+np.save('eval_X.npy', eval_X)
+np.save('eval_Y.npy', eval_Y)
+
+print("Process Complete")
+    #fen_vectors.append(Fen.parse())
+
+i=0
+mate_input_vectors= []
+mate_output = []
+with open('mates_part_1.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)
+
+    for info in reader:
+
+        #print(info[0])
+        f = FENParser(info[0])
+        vector = f.parse()
+        #print(vector)
+        mate_num = int(info[1])
+        #print(mate_num)
+
+        mate_input_vectors.append(vector)
+        mate_output.append(mate_num)
+        i += 1
+        if i % 10000== 0:
+            print(i/1000000)
+
+
+print("50 percent done")
+
+with open('non_mates_part_2.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)
+
+    for info in reader:
+
+        #print(info[0])
+        f = FENParser(info[0])
+        vector = f.parse()
+        #print(vector)
+        #print(mate_num)
+
+        mate_input_vectors.append(vector)
+        mate_output.append(0)
+        i += 1
+        if i % 10000 == 0:
+            print(i/1000000)
+
+mate_X = np.array(mate_input_vectors)
+mate_Y = np.array(mate_output)
+
+np.save('mate_X.npy', mate_X)
+np.save('mate_Y.npy', mate_Y)
